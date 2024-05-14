935edc2594534c3f98b443e243ac808e56d6c4c4
Syedmin <syed.min.khan777@gmail.com>
2024-05-14 16:19:17 +0500

    Adding fectch method with useEffect Hook

--- components/CountriesContainer.jsx
+++ components/CountriesContainer.jsx
@@ -1,26 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import CountaryCard from "./CountaryCard";
-import countriesData from "../countriesData";
+
 const CountriesContainer = ({ query }) => {
+  // State to hold the fetched countries data
+  const [countriesData, setCountriesData] = useState([]);
+
+  // useEffect to fetch data from the API when the component mounts
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        // Update the countriesData state with the fetched data
+        setCountriesData(data);
+      });
+  }, []); // Empty dependency array means this effect runs only once after the initial render
+
+  // useMemo to memoize the filteredCountries calculation
+  // This calculation runs only when countriesData or query changes
+  const filteredCountries = useMemo(() => {
+    return countriesData.filter((country) =>
+      // Convert country name and query to lowercase for case-insensitive comparison
+      country.name.common.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [countriesData, query]); // Dependencies array for useMemo
+
   return (
     <>
+      {/* Display the total count of filtered countries */}
+      <div>Total Count: {filteredCountries.length}</div>
       <div className="countries-container">
-        {countriesData
-          .filter((country) =>
-            country.name.common.toLowerCase().includes(query)
-          )
-          .map((country) => {
-            return (
-              <CountaryCard
-                key={country.name.common}
-                name={country.name.common}
-                flag={country.flags.svg}
-                population={country.population}
-                region={country.region}
-                capital={country.capital?.[0]}
-              />
-            );
-          })}
+        {/* Map over the filtered countries and render a CountaryCard for each */}
+        {filteredCountries.map((country) => (
+          <CountaryCard
+            key={country.name.common}
+            name={country.name.common}
+            flag={country.flags.svg}
+            population={country.population}
+            region={country.region}
+            capital={country.capital?.[0]}
+          />
+        ))}
       </div>
     </>
   );
