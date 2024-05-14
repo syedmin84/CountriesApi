9a74d9319f51d71fdd6fd9fbd5c57d5f29d6c30e
Syedmin <syed.min.khan777@gmail.com>
2024-05-14 13:00:55 +0500

    Add Search Quey Functionality

--- components/CountriesContainer.jsx
+++ components/CountriesContainer.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import CountaryCard from "./CountaryCard";
 import countriesData from "../countriesData";
-const CountriesContainer = () => {
+const CountriesContainer = ({ query }) => {
   return (
-    <div className="countries-container">
-      {countriesData.map((country) => {
-        return (
-          <CountaryCard
-            key={country.name.common}
-            name={country.name.common}
-            flag={country.flags.svg}
-            population={country.population}
-            region={country.region}
-            capital={country.capital?.[0]}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="countries-container">
+        {countriesData
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountaryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+              />
+            );
+          })}
+      </div>
+    </>
   );
 };
 
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 // Alt + ⬆⬇
-const SearchBar = () => {
+// ctr + Space for import library
+const SearchBar = ({ setQuery }) => {
   return (
     <div className="search-container">
-     <i className="fa-solid fa-magnifying-glass"></i>
-      <input type="text" placeholder="Search for a country..." />
+      <i className="fa-solid fa-magnifying-glass"></i>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
     </div>
   );
 };
